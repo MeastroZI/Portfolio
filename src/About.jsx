7655382e29d6959a356ed97ddb6384840a25ec64
Vinit <vinitsharma2480@gmail.com>
2023-07-29 23:47:05 +0530

    updating skills effect and typewriter effect text

--- src/About.jsx
+++ src/About.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
-import  "./Style/AboutStyle.css";
+import "./Style/AboutStyle.css";
 
 import elementObserver from './ElementObserver';
 // import styled from 'styled-components';
-import { useRef ,useEffect} from 'react';
+import { useRef, useEffect } from 'react';
 import img from './img/myIMG.png'
 
 
 
-function About(){
+function About() {
     const SklRef = useRef(null)
-    
+
     const SklVisiable = elementObserver(SklRef)
 
-    
+
     // const TxtVisiable = elementObserver()
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         const SklLogos = SklRef.current.querySelectorAll('.AboutSkills')
         const AbtTxt = document.getElementsByClassName('AbtText')[0];
-        if (SklVisiable)    {
-            SklLogos.forEach((e)=>{
+        if (SklVisiable) {
+            SklLogos.forEach((e) => {
                 e.classList.remove('InVisiable')
             })
-        
-            AbtTxt.classList.add('Visiable') 
-            
+
+            AbtTxt.classList.add('Visiable')
+
         }
-        return ()=>{
-            SklLogos.forEach((e)=>{
+        return () => {
+            SklLogos.forEach((e) => {
                 e.classList.add('InVisiable')
             })
             AbtTxt.classList.remove('Visiable')
         }
-        },[SklVisiable])
-  
-    return(
-        
+    }, [SklVisiable])
+
+    return (
+
         <div className="AboutContainer" id='About' >
             <div className='Before_Design' id='AboutB'>ABOUT</div>
             <div className='Before_Design' id='MeB'>ME</div>
@@ -50,10 +50,10 @@ function About(){
                         {/* <img src={img} alt="" /> */}
                         Hello, I'm Vinit Sharma and I'm a self-taught frontend web developer. I am passionate about learning new technologies of the computer world to make the internet world more beautiful.  Currently I am the Computer science student in Parul university Vadodara Gujarat. I spend the majority of my remaining time  in VS-Code , playing games , searching queries on google reading documentation  or watching videos on youtube
                     </span>
-                    
+
 
                 </div>
-                
+
 
 
                 <div className="Skilleffect" >
@@ -61,19 +61,20 @@ function About(){
                     <div className="effectContainer" >
 
                         <i class="fa-solid fa-brain center"></i>
-                        
+
                         <li className='AboutSkills  html InVisiable' ><i className="fa-brands fa-html5" ></i> </li>
 
-                        <li className='AboutSkills  css InVisiable'><i className="fa-brands fa-css3-alt"></i></li>  
+                        <li className='AboutSkills  css InVisiable'><i className="fa-brands fa-css3-alt"></i></li>
 
                         <li className='AboutSkills  js InVisiable'><i className="fa-brands fa-js" ></i></li>
 
                         <li className='AboutSkills  react InVisiable'><i className="fa-brands fa-react "></i></li>
-                       
+
+                        <li className='AboutSkills  node InVisiable'><i className="fa-brands fa-node-js "></i></li>
                     </div>
                 </div>
             </div>
-           
+
         </div>
     )
 }
